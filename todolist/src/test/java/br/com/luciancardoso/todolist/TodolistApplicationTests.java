193a081280809f6e1a416f60e2c442728eedb4ee
Lucian Cardoso <luciancardoso20@gmail.com>
2024-01-21 19:25:37 -0400

    Criando Tabela Usuário

---
+++ todolist/src/test/java/br/com/luciancardoso/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luciancardoso.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
